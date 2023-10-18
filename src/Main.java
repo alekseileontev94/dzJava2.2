53e23e70c39c576e64ffa19d5dfabb9930c5e024
Aleksei Leontev <aleksei_leontev94@mail.ru>
2023-10-18 22:54:24 +0500

    untitled2

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,14 @@ public class Main {
         System.out.println("расчет бонусов за пополнение счета связи");
         int check = 100; //счет клиента
         int x = 1000;
-        int a = 3200; //пополнение счета
-        int y = 100; //дарит ему по 1 рублю за каждые полные 100 рублей пополнения
-        if (a > x) {
-            System.out.println((check + a + (a / y)) + " пополнение счета " + "+" + " Бонус за пополнение счета ");
+        int refill = 3800; //пополнение счета
+        int bonus = 100; //дарит ему по 1 рублю за каждые полные 100 рублей пополнения
+        if (refill > x) {
+            System.out.println((check + refill) + " пополнение счета "  + ( refill/bonus ) + " + Бонус за пополнение счета "
+                    + " баланс = " + ((check + refill) + ( refill/bonus ) ));
 
         } else {
-            System.out.println(check + a + " пополнение счета ");
+            System.out.println(check + refill + " пополнение счета ");
         }
 
     }
